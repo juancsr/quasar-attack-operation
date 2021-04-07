a24919153f3d5ece71c39f5761835d17e9c26b2a
juancsr <juancsr@protonmail.com>
2021-04-06 20:26:17 -0500

    improving http constants names

--- constants/http_responses.go
+++ constants/http_responses.go
@@ -2,5 +2,5 @@ package constants
 
 import "net/http"
 
-const OK int = http.StatusOK
-const ERROR_CODE_404 int = http.StatusNotFound
+const OK_200 int = http.StatusOK
+const NotFound_404 int = http.StatusNotFound
