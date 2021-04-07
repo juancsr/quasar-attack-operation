9b1208990fc47980743fa9b9ffce3a9a11ab0219
juancsr <juancsr@protonmail.com>
2021-04-06 20:05:25 -0500

    turning satellites const into a map

--- constants/satellites.go
+++ constants/satellites.go
@@ -1,6 +1,13 @@
 package constants
 
-// Satelites
-var KENOBI = [2]int{-500, 200}
-var SKYWALKER = [2]int{100, -100}
-var SATO = [2]int{500, 100}
+var Satellites map[string][2]float32
+
+func init() {
+	// Satelites
+	_satellites := map[string][2]float32{
+		"kenobi":    {-500.0, 200.0},
+		"skywalker": {100.0, -100.0},
+		"sato":      {500.0, 100.0},
+	}
+	Satellites = _satellites
+}
