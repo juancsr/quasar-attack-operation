1b6e7c411783398d5493be40716a02168d28ea80
juancsr <juancsr@protonmail.com>
2021-04-06 20:05:08 -0500

    default route improved

--- routers/routers.go
+++ routers/routers.go
@@ -4,15 +4,14 @@ import "github.com/gin-gonic/gin"
 
 var router = gin.Default()
 
+// Manage the default API route (which is /)
 func defaultRouters() {
-	router.GET("/", func(c *gin.Context) { c.JSON(200, gin.H{"message": "Unauthorized user"}) })
-	router.POST("/", func(c *gin.Context) { c.JSON(200, gin.H{"message": "Unauthorized user"}) })
+	router.Any("/", func(c *gin.Context) { c.JSON(200, gin.H{"message": "Unauthorized user"}) })
 }
 
 /* Default go function
 initialize to all endpoints*/
 func init() {
-	// Default routes
 	defaultRouters()
 	// Endpoints
 	topSecret(router)
