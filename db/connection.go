f2e3888a53c04a5f38244acbc1a5df2034101997
juancsr <juancsr@protonmail.com>
2021-04-11 15:07:12 -0500

    mongo connection and modules added

---
+++ db/connection.go
@@ -0,0 +1,57 @@
+package db
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"os"
+
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+//CTX Context variable
+var CTX = context.TODO()
+
+var client *mongo.Client
+
+// var collection *mongo.Collection
+
+// init connection
+func init() {
+	var err error
+	// Docker URI
+	USER := os.Getenv("USER")
+	PASSWORD := os.Getenv("PASSWORD")
+	DB := os.Getenv("DB")
+	CLUSTER := os.Getenv("CLUSTER")
+	URI := "mongodb+srv://" + USER + ":" + PASSWORD + "@" + CLUSTER + "/" + DB + "?retryWrites=true&w=majority"
+	fmt.Println(URI)
+	clientOptions := options.Client().ApplyURI(URI)
+	client, err = mongo.Connect(CTX, clientOptions)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+	err = client.Ping(CTX, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("connected sucefully")
+}
+
+// cliente de db
+func GetCollection(collectionName string) *mongo.Collection {
+	client.Connect(CTX)
+	collection := client.Database(os.Getenv("DB")).Collection(collectionName)
+	return collection
+}
+
+// cierra la conexión con la BD
+func CloseConnection() {
+	defer func() {
+		if err := client.Disconnect(CTX); err != nil {
+			log.Panicln(err.Error())
+		}
+	}()
+}
--- go.mod
+++ go.mod
@@ -11,6 +11,7 @@ require (
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	github.com/modern-go/reflect2 v1.0.1 // indirect
 	github.com/ugorji/go v1.2.5 // indirect
+	go.mongodb.org/mongo-driver v1.5.1
 	golang.org/x/crypto v0.0.0-20210322153248-0c34fe9e7dc2 // indirect
 	golang.org/x/sys v0.0.0-20210403161142-5e06dd20ab57 // indirect
 	gonum.org/v1/gonum v0.9.1
